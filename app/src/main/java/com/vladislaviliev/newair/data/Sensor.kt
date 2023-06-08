19f71bbe9aca4e4e482ca5e5f9d9414e269a642a
Vladislav Iliev <vladislav.iliev@hotmail.com>
2023-06-08 15:53:55 +0300

    Sensors become non-Comparable

--- app/src/main/java/com/vladislaviliev/newair/data/Sensor.kt
+++ app/src/main/java/com/vladislaviliev/newair/data/Sensor.kt
@@ -2,6 +2,4 @@ package com.vladislaviliev.newair.data
 
 import com.google.android.gms.maps.model.LatLng
 
-data class Sensor(val type: SensorType, val latLng: LatLng, val measure: Double) : Comparable<Sensor> {
-    override fun compareTo(other: Sensor) = measure.compareTo(other.measure)
-}
+data class Sensor(val type: SensorType, val latLng: LatLng, val measure: Double)

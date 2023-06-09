e3bfe1fca84e9bfbe2a0873c0ee21d7fe4965380
Vladislav Iliev <vladislav.iliev@hotmail.com>
2023-06-09 23:00:05 +0300

    In Map View, toggle visibility of legend via Group

--- app/src/main/java/com/vladislaviliev/newair/fragments/map/CircleMode.kt
+++ app/src/main/java/com/vladislaviliev/newair/fragments/map/CircleMode.kt
@@ -1,85 +1,53 @@
 package com.vladislaviliev.newair.fragments.map
 
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.preference.PreferenceManager
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.CircleOptions
 import com.vladislaviliev.newair.R
 import com.vladislaviliev.newair.Vm
-import com.vladislaviliev.newair.data.Sensor
 import com.vladislaviliev.newair.data.SensorType
 import com.vladislaviliev.newair.data.getColor
 
-internal class CircleMode(fragment: MapFragment, private val googleMap: GoogleMap) {
-
-    private val legendBackground = fragment.requireView().findViewById<View>(R.id.legendBackground)
-    private val legendBoxes = mutableListOf<View>()
-    private val legendTexts = mutableListOf<View>()
-    private val isColorBlind = PreferenceManager.getDefaultSharedPreferences(fragment.requireContext())
-        .getBoolean(fragment.resources.getString(R.string.color_blind_switch_key), false)
+internal class CircleMode(fragment: MapFragment, googleMap: GoogleMap) {
 
     init {
-        initializeLegend(fragment.requireView())
-
         val viewModel: Vm by (fragment.activityViewModels())
-        fragment.requireView().findViewById<View>(R.id.refreshButton).apply {
+        val root = fragment.requireView()
+        val legendContainer = root.findViewById<View>(R.id.legend_container)
+        val isColorBlind = PreferenceManager.getDefaultSharedPreferences(fragment.requireContext())
+            .getBoolean(fragment.resources.getString(R.string.color_blind_switch_key), false)
+        root.findViewById<View>(R.id.legendBlue).setBackgroundColor(getColor(isColorBlind, 0))
+        root.findViewById<View>(R.id.legendGreenLight).setBackgroundColor(getColor(isColorBlind, 1))
+        root.findViewById<View>(R.id.legendGreen).setBackgroundColor(getColor(isColorBlind, 2))
+        root.findViewById<View>(R.id.legendGreenDark).setBackgroundColor(getColor(isColorBlind, 3))
+        root.findViewById<View>(R.id.legendOrangeLight).setBackgroundColor(getColor(isColorBlind, 4))
+        root.findViewById<View>(R.id.legendOrange).setBackgroundColor(getColor(isColorBlind, 5))
+        root.findViewById<View>(R.id.legendOrangeDark).setBackgroundColor(getColor(isColorBlind, 6))
+        root.findViewById<View>(R.id.legendRedLight).setBackgroundColor(getColor(isColorBlind, 7))
+        root.findViewById<View>(R.id.legendRed).setBackgroundColor(getColor(isColorBlind, 8))
+        root.findViewById<View>(R.id.legendRedDark).setBackgroundColor(getColor(isColorBlind, 9))
+        root.findViewById<View>(R.id.legendPurple).setBackgroundColor(getColor(isColorBlind, 10))
+
+        root.findViewById<View>(R.id.refreshButton).apply {
             visibility = View.VISIBLE
             setOnClickListener { viewModel.downloadData() }
         }
-        fragment.requireView().findViewById<View>(R.id.legendButton).apply {
+        root.findViewById<View>(R.id.legendButton).apply {
             visibility = View.VISIBLE
-            setOnClickListener { toggleMapLegend() }
+            setOnClickListener { legendContainer.visibility = if (legendContainer.isVisible) View.GONE else View.VISIBLE }
         }
-        viewModel.liveSensors.observe(fragment) { onNewData(it) }
-    }
 
-    private fun onNewData(sensors: Iterable<Sensor>) {
-        googleMap.clear()
-        val circleOptions = CircleOptions().radius(100.toDouble()).strokeWidth(0.toFloat())
-        sensors.filter { it.type == SensorType.PM10 }.forEach {
-            circleOptions.center(it.latLng)
-            circleOptions.fillColor(getColor(isColorBlind, it.measure, 150))
-            googleMap.addCircle(circleOptions)
+        val circleBuilder = CircleOptions().radius(100.toDouble()).strokeWidth(0.toFloat())
+        viewModel.liveSensors.observe(fragment) { sensors ->
+            googleMap.clear()
+            sensors.filter { it.type == SensorType.PM10 }.forEach {
+                circleBuilder.center(it.latLng)
+                circleBuilder.fillColor(getColor(isColorBlind, it.measure, 150))
+                googleMap.addCircle(circleBuilder)
+            }
         }
-    }
-
-    private fun initializeLegend(root: View) {
-        legendBoxes.add(root.findViewById(R.id.legendBlue))
-        legendBoxes.add(root.findViewById(R.id.legendGreenLight))
-        legendBoxes.add(root.findViewById(R.id.legendGreen))
-        legendBoxes.add(root.findViewById(R.id.legendGreenDark))
-        legendBoxes.add(root.findViewById(R.id.legendOrangeLight))
-        legendBoxes.add(root.findViewById(R.id.legendOrange))
-        legendBoxes.add(root.findViewById(R.id.legendOrangeDark))
-        legendBoxes.add(root.findViewById(R.id.legendRedLight))
-        legendBoxes.add(root.findViewById(R.id.legendRed))
-        legendBoxes.add(root.findViewById(R.id.legendRedDark))
-        legendBoxes.add(root.findViewById(R.id.legendPurple))
-        legendTexts.add(root.findViewById(R.id.legendTextBlue))
-        legendTexts.add(root.findViewById(R.id.legendTextGreenLight))
-        legendTexts.add(root.findViewById(R.id.legendTextGreen))
-        legendTexts.add(root.findViewById(R.id.legendTextGreenDark))
-        legendTexts.add(root.findViewById(R.id.legendTextOrangeLight))
-        legendTexts.add(root.findViewById(R.id.legendTextOrange))
-        legendTexts.add(root.findViewById(R.id.legendTextOrangeDark))
-        legendTexts.add(root.findViewById(R.id.legendTextRedLight))
-        legendTexts.add(root.findViewById(R.id.legendTextRed))
-        legendTexts.add(root.findViewById(R.id.legendTextRedDark))
-        legendTexts.add(root.findViewById(R.id.legendTextPurple))
-        legendTexts.add(root.findViewById(R.id.measurement))
-        legendBoxes.indices.forEach { legendBoxes[it].setBackgroundColor(getColor(isColorBlind, it)) }
-    }
-
-    private fun toggleMapLegend() {
-        if (legendBackground.visibility == View.VISIBLE) {
-            legendBackground.visibility = View.GONE
-            legendBoxes.forEach { it.visibility = View.GONE }
-            legendTexts.forEach { it.visibility = View.GONE }
-            return
-        }
-        legendBackground.visibility = View.VISIBLE
-        legendBoxes.forEach { it.visibility = View.VISIBLE }
-        legendTexts.forEach { it.visibility = View.VISIBLE }
     }
 }

2da003d5d9dbfc652f4979e8446eaace92dbe901
Vladislav Iliev <vladislav.iliev@hotmail.com>
2023-06-29 19:40:43 +0300

    In Maths extract type filtering to getReading()

--- app/src/main/java/com/vladislaviliev/newair/sensor/Maths.kt
+++ app/src/main/java/com/vladislaviliev/newair/sensor/Maths.kt
@@ -7,14 +7,13 @@ object Maths {
     private fun distanceBetween(a: LatLng, b: LatLng) = Location("a").apply { latitude = a.latitude; longitude = a.longitude }
         .distanceTo(Location("b").apply { latitude = b.latitude; longitude = b.longitude })
 
-    private fun getAverage(sensors: List<Sensor>, type: SensorType) =
-        (sensors.filter { it.type == type }.sumOf { it.measure } / sensors.size).toInt().toDouble()
+    private fun getAverage(sensors: Collection<Sensor>) = (sensors.sumOf { it.measure } / sensors.size).toInt().toDouble()
 
-    private fun closestReading(latLng: LatLng, sensors: List<Sensor>, type: SensorType) = sensors
-        .filter { it.type == type }
-        .minWith { a, b -> (distanceBetween(latLng, a.latLng) - distanceBetween(latLng, b.latLng)).toInt() }
-        .measure
+    private fun closestReading(latLng: LatLng, sensors: Iterable<Sensor>) =
+        sensors.minWith { a, b -> (distanceBetween(latLng, a.latLng) - distanceBetween(latLng, b.latLng)).toInt() }.measure
 
-    fun getReading(latLng: LatLng?, sensors: List<Sensor>, type: SensorType) =
-        if (latLng == null) getAverage(sensors, type) else closestReading(latLng, sensors, type)
+    fun getReading(latLng: LatLng?, sensors: List<Sensor>, type: SensorType): Double {
+        val filteredSensors = sensors.filter { it.type == type }
+        return if (latLng == null) getAverage(filteredSensors) else closestReading(latLng, filteredSensors)
+    }
 }

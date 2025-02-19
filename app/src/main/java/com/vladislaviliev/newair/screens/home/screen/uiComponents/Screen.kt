61ae90c48ace7ee393955503b26cd6c39004a1b4
Vladislav Iliev <vladislav.iliev@hotmail.com>
2025-02-19 15:29:22 +0200

    Move Home state color resolution to the UI; the compose compiler wasn't added to the 'air' project

--- app/src/main/java/com/vladislaviliev/newair/screens/home/screen/uiComponents/Screen.kt
+++ app/src/main/java/com/vladislaviliev/newair/screens/home/screen/uiComponents/Screen.kt
@@ -7,14 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.takeOrElse
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.vladislaviliev.air.readings.calculations.Health
 import com.vladislaviliev.air.readings.downloader.metadata.Metadata
 import com.vladislaviliev.air.readings.downloader.responses.LiveResponse
 import com.vladislaviliev.air.user.location.UserLocation
@@ -65,8 +66,8 @@ private fun Screen(
 ) {
     Surface(
         modifier.fillMaxSize(),
-        color = Health.checkUnspecifiedBackgroundColor(backgroundColor),
-        contentColor = Health.checkUnspecifiedContentColor(contentColor)
+        color = backgroundColor.takeOrElse(MaterialTheme.colorScheme::surface),
+        contentColor = contentColor.takeOrElse(MaterialTheme.colorScheme::surface)
     ) {
         BackgroundImages(Modifier.fillMaxSize())
 
